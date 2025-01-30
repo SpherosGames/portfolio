26a39ad5520e171f61bdc4081f8a603ac570582c
SpherosGames <jbol1@st.noorderpoort.nl>
2025-01-30 11:49:56 +0100

    Made code bits a component

--- Components/CodeShowcase.js
+++ Components/CodeShowcase.js
@@ -1,18 +1,16 @@
 
-function renderCode(text) {
+function renderCode(text, title, warning) {
     return `
         <div class="w-[80%] mx-auto bg-[#007ea7] rounded m-5 p-5">
             <div class="flex flex-row justify-around w-[50%] m-auto">
-                <p class="text-3xl">AIData code</p><br/>
+                <p class="text-3xl">${title}</p><br/>
                 <button class="rounded bg-[#00a8e8] p-5">Show</button>
             </div>
             <pre>
                 <code data-language="csharp">${text}</code>
             </pre>
-
-            <p>
-                Note: Couldn't remove &lt;/gamemanager&gt; code bececuase of the library I am using.
-            </p>
+            <br>
+            <p>${warning}</p>
         </div>
     `;
 }
@@ -20,8 +18,10 @@ function renderCode(text) {
 // Function to render the card based on data attributes from an HTML element
 function renderCardFromElement(element) {
     const text = element.dataset.text;
-
-    element.innerHTML = renderCode(text);
+    const title = element.dataset.title;
+    const warning = element.dataset.warning;
+    
+    element.innerHTML = renderCode(text, title, warning);
 }
 
 // Automatically render all elements with the class "square-card"
